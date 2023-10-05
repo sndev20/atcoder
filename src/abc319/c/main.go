87cc42a22b3a433d574ba8c51bd8c79a6fd0df61
sndev20 <s.nozaki.dev@gmail.com>
2023-10-05 19:55:51 +0900

    abc319/c

--- src/abc319/c/main.go
+++ src/abc319/c/main.go
@@ -14,10 +14,53 @@ var wtr = bufio.NewWriter(os.Stdout)
 
 func main() {
 	defer flush()
-	lines := getStdin()
+	c := [9]int{}
+	for i := range c {
+		fmt.Scan(&c[i])
+	}
 
-	ans := 0
-	fmt.Fprintln(wtr, ans)
+	o := make([]int, 9)
+	for i := range o {
+		o[i] = i
+	}
+
+	// 縦・横・斜めの列の一覧
+	vec := [][]int{
+		{0, 1, 2}, // 上から 1 行目
+		{3, 4, 5}, // 上から 2 行目
+		{6, 7, 8}, // 上から 3 行目
+		{0, 3, 6}, // 左から 1 列目
+		{1, 4, 7}, // 左から 2 列目
+		{2, 5, 8}, // 左から 3 列目
+		{0, 4, 8}, // 左上から右下
+		{2, 4, 6}, // 右上から左下
+	}
+
+	var t int
+	var r int
+	for {
+		var disapointed bool
+		for _, v := range vec {
+			if c[v[0]] == c[v[1]] && o[v[0]] < o[v[2]] && o[v[1]] < o[v[2]] {
+				disapointed = true
+				break
+			} else if c[v[1]] == c[v[2]] && o[v[1]] < o[v[0]] && o[v[2]] < o[v[0]] {
+				disapointed = true
+				break
+			} else if c[v[2]] == c[v[0]] && o[v[2]] < o[v[1]] && o[v[0]] < o[v[1]] {
+				disapointed = true
+				break
+			}
+		}
+		if !disapointed { // がっかりしない
+			r++
+		}
+		t++ // 総数
+		if !nextPermutation(sort.IntSlice(o)) {
+			break
+		}
+	}
+	fmt.Fprintf(wtr, "%.9f\n", (float64(r) / float64(t)))
 }
 
 // ////////////////////////////////////////////////////////////////
